e8eea5b9b8fd7b5921768bf5fecdca2eaf6c8021
Barbara Ayres <ayresbarbara2@gmail.com>
2022-07-07 14:46:06 -0300

    Java

---
+++ Java/PrimeiroJava/src/ExerciciosPlataforma/Aula4_PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package ExerciciosPlataforma;
+
+public class Aula4_PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\nHello, World!!!");
+
+	}
+
+}
