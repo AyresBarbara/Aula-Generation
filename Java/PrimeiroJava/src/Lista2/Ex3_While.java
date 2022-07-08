89c6aa59111e6a2609f964120fd0641e4e4dc93a
Barbara Ayres <ayresbarbara2@gmail.com>
2022-07-08 17:21:33 -0300

    Java

--- Java/PrimeiroJava/src/Lista2/Ex3_While.java
+++ Java/PrimeiroJava/src/Lista2/Ex3_While.java
@@ -1,9 +1,29 @@
 package Lista2;
 
+import java.util.Scanner;
+
 public class Ex3_While {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int mv=0, mc=0, idade;
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.print("\nEntre com sua idade: ");
+		idade = leia.nextInt();
+		while(idade!=-99)
+		{
+			if (idade<21)
+				mv++;//Não se faz necessário abrir chave pq só tem 1 linha
+			if (idade>50)
+				mc++;
+		System.out.print("\nEntre com sua idade: ");
+			idade = leia.nextInt();
+		} 
+		System.out.print("\nO total de pessoas com menos de 21 é: "+mv);
+		System.out.print("\nO total de pessoas com meais de 50 é: "+mc);
+		
+		
 
 	}
 
---
+++ Java/PrimeiroJava/src/Lista2/Ex6.java
@@ -0,0 +1,23 @@
+package Lista2;
+
+import java.util.Scanner;
+
+public class Ex6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner ler=new Scanner(System.in);
+		int n1, soma=0, cont=0;
+		do
+		{
+		System.out.println("\nEntre com um numero: ");
+		n1 = ler.nextInt();
+			if(n1%3==0 && n1!=0)
+			{
+			soma = soma+n1;
+			cont++;
+			}
+		}while(n1!=0);
+			System.out.println("\nA media dos multiplos de 3: "+soma/cont);
+	}
+}
