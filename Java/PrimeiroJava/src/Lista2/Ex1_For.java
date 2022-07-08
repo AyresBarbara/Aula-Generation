6d048f1a33ba001b26a20c74db7b143a06932d54
Barbara Ayres <ayresbarbara2@gmail.com>
2022-07-08 12:49:02 -0300

    Java

---
+++ Java/PrimeiroJava/src/Lista2/Ex1_For.java
@@ -0,0 +1,19 @@
+package Lista2;
+
+public class Ex1_For {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		
+		for(n=1000; n<=1999; n++) 
+		{
+			if (n%11==5)
+			{
+				System.out.println("\nN="+n);
+			}
+		}
+
+	}
+
+}
