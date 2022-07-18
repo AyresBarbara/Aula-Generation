5239f30cd08e36a8d1c3661ba0bbcb8225fed8b8
Barbara Ayres <ayresbarbara2@gmail.com>
2022-07-18 17:28:39 -0300

    Java

---
+++ Vestibular/src/Projeto/Usuário.java
@@ -0,0 +1,31 @@
+package Projeto;
+
+public class Usuário {
+
+	public static void main(String[] args) {
+		
+		Humanas usuario1 = new Humanas("Paloma");
+		Exatas usuario2 = new Exatas ("aluno");
+		
+		
+		usuario1.area("curso");
+		usuario1.setDataNasc("09/06/1994");
+		usuario1.setCPF("123.123.123-12");
+		usuario1.setConcluida(true);
+		usuario1.InscricaoConcluida();
+		System.out.println();
+		usuario1.formulario();
+		
+		usuario2.area("curso");
+		usuario2.setDataNasc("09/06/1994");
+		usuario2.setConcluida(false);
+		usuario2.InscricaoConcluida();
+		System.out.println();
+		
+		
+		
+	
+	}
+
+}
+
