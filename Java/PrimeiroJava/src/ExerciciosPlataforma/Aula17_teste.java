90f80edde983b8b9cd7f6816bcba214d47b2beb3
Barbara Ayres <ayresbarbara2@gmail.com>
2022-07-20 20:58:01 -0300

    Java

---
+++ Java/PrimeiroJava/src/ExerciciosPlataforma/Aula17_teste.java
@@ -0,0 +1,11 @@
+package ExerciciosPlataforma;
+
+public class Aula17_teste {
+
+	public static void main(String[] args) {
+		Aula17_MetodoSobrecarregado metodoTeste = new Aula17_MetodoSobrecarregado();
+		metodoTeste.testaMetodosSobrecarregados();
+
+	}
+
+}
