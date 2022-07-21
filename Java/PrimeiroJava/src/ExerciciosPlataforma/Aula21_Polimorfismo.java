695716f3b52dd87faa8bba57e372406ae4b6aa86
Barbara Ayres <ayresbarbara2@gmail.com>
2022-07-20 23:23:02 -0300

    Java

---
+++ Java/PrimeiroJava/src/ExerciciosPlataforma/Aula21_Polimorfismo.java
@@ -0,0 +1,17 @@
+package ExerciciosPlataforma;
+
+public class Aula21_Polimorfismo {
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(final String nome) {
+		this.nome = nome;
+	}
+
+
+	
+
+}
