4d1a35d41d84ac883ac12b81e93814dd90059938
Barbara Ayres <ayresbarbara2@gmail.com>
2022-07-20 21:58:14 -0300

    Java

---
+++ Java/PrimeiroJava/src/ExerciciosPlataforma/Aula18_Teste.java
@@ -0,0 +1,15 @@
+package ExerciciosPlataforma;
+
+public class Aula18_Teste {
+
+	public static void main(String[] args) {
+		Aula18_Encapsulamento func = new Aula18_Encapsulamento();
+		func.setNome("Bárbara");
+		func.setSalario(2500);
+		
+		System.out.println(func.getNome());
+		System.out.println(func.getSalario());
+
+	}
+
+}
